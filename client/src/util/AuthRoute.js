7df379c81bd5a5804dcef992702b8e80832ebb08
Oskar Lindblad <lindbladoskar@gmail.com>
2021-01-01 14:58:16 +0100

    backup trainig

--- client/src/util/AuthRoute.js
+++ client/src/util/AuthRoute.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, Redirect, Router } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "../context/auth";
 
 function AuthRoute({ component: Component, ...rest }) {
